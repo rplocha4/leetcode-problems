7d50f878cbb0c03f4c952436e9c70f1833caecc3
Radek <plocharadoslaw@gmail.com>
2023-06-28 21:44:22 +0200

    lowest common ancestor of a binary search tree problem

---
+++ lowestCommonAncestor.py
@@ -0,0 +1,16 @@
+def lowestCommonAncestor(root, p, q):
+    """
+    :type root: TreeNode
+    :type p: TreeNode
+    :type q: TreeNode
+    :rtype: TreeNode
+    """
+    curr = root
+
+    while curr:
+        if p.val > curr.val and q.val > curr.val:
+            curr = curr.right
+        elif p.val < curr.val and q.val < curr.val:
+            curr = curr.left
+        else:
+            return curr
