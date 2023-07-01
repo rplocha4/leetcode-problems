fa085ed0ffb8b236cab46b526397d7e488a34f8b
Radek <plocharadoslaw@gmail.com>
2023-07-01 20:58:32 +0200

    k closest points to origin problem

---
+++ kClosest.py
@@ -0,0 +1,12 @@
+def kClosest(points, k):
+    pts = []
+    for x, y in points:
+        dist = (abs(x - 0) ** 2) + (abs(y - 0) ** 2)
+        pts.append([dist, x, y])
+
+    res = []
+    heapq.heapify(pts)
+    for i in range(k):
+        dist, x, y = heapq.heappop(pts)
+        res.append([x, y])
+    return res
